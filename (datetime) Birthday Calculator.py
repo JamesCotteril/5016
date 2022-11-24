fec60729ddce3bfc709b626dbc0927c8dd52e086
JamesCotteril <116466523+JamesCotteril@users.noreply.github.com>
2022-11-25 11:12:41 +1300

    Update and rename datetest.py to (datetime) Birthday Calculator.py

--- (datetime) Birthday Calculator.py
+++ (datetime) Birthday Calculator.py
@@ -6,17 +6,21 @@
 from datetime import datetime
 from datetime import timedelta
 
-
+#Examples of some simple date calculations for future referral
 print("125 days ago it was", datetime.today() - timedelta(days=125), ".\n")
 print("In 2 weeks it will be", datetime.today() + timedelta(weeks=1), ".\n")
 
+#Input 2 birthdays
 birthday_input1 = input("Please enter the first birthday in the format DD Mmm YYYY: \n")
 birthday_input2 = input("Please enter the second birthday in the format DD Mmm YYYY: \n")
 
+#Cast to strptime for calculation
 birthday_object1 = datetime.strptime(birthday_input1, '%d %b %Y')
 birthday_object2 = datetime.strptime(birthday_input2, '%d %b %Y')
 
+#Find oldest and youngest
 oldest = min(birthday_object1, birthday_object2)
 youngest = max(birthday_object1, birthday_object2)
 
+#Print the calculation
 print("The difference in age is approximately", youngest.year - oldest.year, "years")
