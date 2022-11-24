628d3e6d876ebe090a86f55717893d1439b2e374
JamesCotteril <116466523+JamesCotteril@users.noreply.github.com>
2022-11-25 11:29:00 +1300

    Update and rename functions2.py to (functions) functions_practice2.py

--- (functions) functions_practice2.py
+++ (functions) functions_practice2.py
@@ -1,4 +1,4 @@
-# Functions3.py
+# (functions) functions_practice2.py
 #
 # @ author: J T Cotterill
 # date: November 2022
@@ -26,22 +26,22 @@
 
 #Challenge 6 - Complete
 
-#stored_list = [21, 32, 43, 22, 1, 5, 18]
+stored_list = [21, 32, 43, 22, 1, 5, 18]
 
-#def only_even_numbers(param_list):
-#    sorted_param_list = sorted(param_list)
-#    for i in sorted_param_list:
-#        if i % 2 == 0:
-#            print(i)
+def only_even_numbers(param_list):
+    sorted_param_list = sorted(param_list)
+    for i in sorted_param_list:
+        if i % 2 == 0:
+            print(i)
             
-#def only_odd_numbers(param_list):
-#    sorted_param_list = sorted(param_list)
-#    for i in sorted_param_list:
-#        if i % 2 != 0:
-#            print(i)
+def only_odd_numbers(param_list):
+    sorted_param_list = sorted(param_list)
+    for i in sorted_param_list:
+        if i % 2 != 0:
+            print(i)
             
-#print("Test case assertion: displays 18, 22, 32")
-#only_even_numbers(stored_list)
+print("Test case assertion: displays 18, 22, 32")
+only_even_numbers(stored_list)
 
-#print("Test case assertion: displays 1, 5, 21, 43")
-#only_odd_numbers(stored_list)
+print("Test case assertion: displays 1, 5, 21, 43")
+only_odd_numbers(stored_list)
