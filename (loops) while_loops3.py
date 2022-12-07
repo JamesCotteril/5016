7ead6ab889a6592e7f1f7eec4a78be51ca276c95
JamesCotteril <116466523+JamesCotteril@users.noreply.github.com>
2022-12-07 17:51:40 +1300

    Update (loops) while_loops3.py

--- (loops) while_loops3.py
+++ (loops) while_loops3.py
@@ -3,24 +3,27 @@
 # J T Cotterill
 # October 2022
 
+#Login attempts
+
+#Set password and number of tries
 password = str('secret')
 number_of_tries = 3
 
 while True:
     attempt = str(input("Please enter your password to proceed...\n"))
-
+#Breaks the loop if the last try is used
     if number_of_tries == 1:
         print('Error. Number of tries exceeded. Terminating...')
         break
-    
+#Password is correct    
     if attempt == 'secret':
         print('Password confirmed. Logging in...')
         break
-
+#Exit if exit is input
     if attempt == 'exit':
         print('Exit confirmed. Terminating...')
         break
-
+#Decrease number of tries if the password is wrong
     else:
         number_of_tries = number_of_tries - 1
         print("Sorry that's incorrect. You have", number_of_tries, "tries remaining. Please try again.\n")
