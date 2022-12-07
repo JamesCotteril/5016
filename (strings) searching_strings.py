005f5d70274276a5362ae47fae7d7fda34bb105a
JamesCotteril <116466523+JamesCotteril@users.noreply.github.com>
2022-12-07 18:04:19 +1300

    Update (strings) searching_strings.py

--- (strings) searching_strings.py
+++ (strings) searching_strings.py
@@ -2,8 +2,9 @@
 #
 #  J T Cotterill
 # November 2022
+#Searching strings will play a big role in the assignment. I collated answers for the questions here. I tested all of them in python.
 
-#Challenge 1 - Complete
+#Challenge 1 - Complete - Count a character in a string
 
 #stored_string ="The first rule of fight club is: do not talk about fight club."
 #counter = 0
@@ -11,19 +12,19 @@
 
 #print("The number of blank spaces is {0}\n".format(stored_string.count(" ")))
 
-#Challenge 3 - Complete
+#Challenge 3 - Complete - Display if a string starts with a certain phrase
 
 #input_string = str(input("Please input some text:\n").title())
 #print("Does this string start with '"+input_string+"'?: {0}\n".format(stored_string.startswith(input_string)))
 
-#Challenge 5 - Complete
+#Challenge 5 - Complete - Find the index number of a character or characters in a string
 
 #fight_index = stored_string.find("fight")
 #second_fight_index = stored_string.find("fight", 29)
 #print("The index position of the first instance of 'fight' is {0}".format(fight_index))
 #print("The index position of second instance of 'fight' is {0}".format(second_fight_index))
 
-#Challenge 9 - Complete
+#Challenge 9 - Complete - Search for at least one capital letter and one number in a password, and validate it.
 
 import re
 password_confirmed = False
@@ -40,7 +41,7 @@ while password_confirmed == False:
         print("Error. Please include a number and a capital letter.")
        continue
 
-#Challenge 7 - Complete
+#Challenge 7 - Complete - make a username using an email address by searching for index numbers
 
 #email_input = input("Please enter your email address:\n")
 #period_index = email_input.find(".")
