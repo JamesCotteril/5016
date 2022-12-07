c5b1314cd4f4d80ba69fbb3207cffe32dc31e724
JamesCotteril <116466523+JamesCotteril@users.noreply.github.com>
2022-12-07 17:28:34 +1300

    Update (integers) shoe_size.py

--- (integers) shoe_size.py
+++ (integers) shoe_size.py
@@ -11,8 +11,11 @@ print("my_size is ", my_size,"\n")
 print("Casting to an integer...\n")
 # variable cast to integer
 
+#Code to round a float to the nearest integer, then cast
 my_size = round(my_size, 0)
 my_size = int(my_size)
  
 print("The type of my_size is now ", type(my_size),"\n")
 print("my_size is ", my_size,"\n")
+
+#I added a round line of code to round a number to cast to an integer. Just experimenting with floats/integers.
